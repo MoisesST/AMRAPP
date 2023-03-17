474eba53731def5ad39e9e50e186fa1d8179633b
MoisesST <moises.dev13@gmail.com>
2023-03-16 21:45:06 -0300

    feat: init project

---
+++ src/Main/index.tsx
@@ -0,0 +1,18 @@
+import {
+  Container,
+  Footer,
+  FooterContainer,
+} from './styles';
+
+export function Main() {
+  return (
+    <>
+      <Container>
+      </Container>
+      <Footer>
+        <FooterContainer>
+        </FooterContainer>
+      </Footer>
+    </>
+  );
+}
