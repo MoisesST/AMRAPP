3b1f7c9a5fd47972f680704a65efc86ad72db05b
alguipires <alguipires@gmail.com>
2023-04-24 17:28:06 -0300

    refactor(rebase main): reabase main

--- app/index.tsx
+++ app/index.tsx
@@ -1,14 +1,19 @@
+import firebaseConfig from "../src/config/firebaseConfig";
+import useFirebase from "../src/hooks/useFirebase";
+//import Router from "../src/screens/Router"; //verificar uso
 import { useFonts } from 'expo-font';
 
 import { useColorScheme } from 'react-native';
 
 import { StatusBar } from 'expo-status-bar';
-
 import { Main } from '../src/Main';
 import themes from '../src/themes';
 import ThemeContextProvider from '../src/contexts/ThemeContext';
 
 function App() {
+  const firebaseApp = useFirebase(firebaseConfig);
+  if (firebaseApp == null) return <div>Loading...</div>;
+
   const [isFontsLoaded] = useFonts({
     'GeneralSans-400': require('../assets/fonts/GeneralSans-Regular.otf'),
     'GeneralSans-600': require('../assets/fonts/GeneralSans-Semibold.otf'),
@@ -21,6 +26,8 @@ function App() {
   if (!isFontsLoaded) {
     return null;
   }
+
+  //return <Router />; //verificar uso
 
   return (
     <ThemeContextProvider>
